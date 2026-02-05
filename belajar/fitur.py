13f3ea6224abd1cd7233ec32b0c6d34d715b6733
kezenu <wsanjaya69@gmail.com>
2026-02-05 23:13:42 +0700

    fkjhwefhnwe

--- belajar/fitur.py
+++ belajar/fitur.py
@@ -4,23 +4,32 @@ import MetaTrader5 as mt5
 import matplotlib.pyplot as plt
 import mplfinance as mpf
 
+#BOT KONFIGURASI
+
 SYMBOL = "EURUSD"
-TIMEFRAME = mt5.TIMEFRAME_H1
-TOTAL_CANDLES = 500
+TIMEFRAME = mt5.TIMEFRAME_M15
+TOTAL_CANDLES = 2000
 
+def get_fitur():
+    if not mt5.initialize():
+        print(f"gagal Terhubung : {mt5.last_error()}")
+        quit()
 
-if not mt5.initialize():
-    print("gagal Terhubung")
-    quit()
+    rates = mt5.copy_rates_from_pos(SYMBOL, TIMEFRAME, 0, TOTAL_CANDLES)
+    mt5.shutdown()
 
-rates = mt5.copy_rates_from_pos(SYMBOL, TIMEFRAME, 0, TOTAL_CANDLES)
-mt5.shutdown()
+    df = pd.DataFrame(rates)
+    df.drop(columns=['spread', 'real_volume',], inplace=True)
+    df['time'] = pd.to_datetime(df['time'], unit='s')
+    df.set_index(df['time'], inplace=True)
 
-df = pd.DataFrame(rates)
-df.drop(columns=['spread', 'real_volume',], inplace=True)
-df['time'] = pd.to_datetime(df['time'], unit='s')
-df.set_index(df['time'], inplace=True)
+    df.rename(columns={'open': 'Open', 'high': 'High', 'low': 'Low', 'close': 'Close', 'tick_volume': 'Volume'}, inplace=True)
+    ap =[mpf.make_addplot(df.ta.sma(length=20), color='blue'),
+      mpf.make_addplot(df.ta.sma(length=50), color='red'),
+      mpf.make_addplot(df.ta.pivot())
+    ] 
 
-df.rename(columns={'open': 'Open', 'high': 'High', 'low': 'Low', 'close': 'Close', 'tick_volume': 'Volume'}, inplace=True)
+    
+    mpf.plot(df, type='candle',style='charles', addplot=ap)
 
-mpf.plot(df, type='candle', style='charles', volume=True, mav=(3,6,9), hlines=[1.1855])
+get_fitur()
